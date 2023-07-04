ba5c6e9e0182b4a818346a6ebad5f1394c7c02af
philippe basuyau <basuyau.philippe@free.fr>
2023-07-04 13:06:29 +0100

    dev logoggermiddleware

--- back/server.js
+++ back/server.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// middle ware
+app.use("/", (req, res, next) => {
+  console.log("req: ", req.method, req.url);
+  //res.send("Hello World!");
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
