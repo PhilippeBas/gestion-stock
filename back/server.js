a1b15c0fa578185e664a21f2927817e5ecc9c47b
philippe basuyau <basuyau.philippe@free.fr>
2023-07-04 14:32:18 +0200

    Installation de typescript -D

--- back/server.js
+++ back/server.js
@@ -1,5 +1,8 @@
+console.log("About to start a server ...");
+
 const express = require("express");
 const serveIndex = require("serve-index");
+
 const app = express();
 const port = 3000;
 const publicDir = ".";
@@ -7,13 +10,11 @@ const publicDir = ".";
 // middle ware
 app.use("/", (req, res, next) => {
   console.log("req: ", req.method, req.url);
-  //res.send("Hello World!");
   next();
 });
 
 // pour servir des fichiers statics
 app.use(express.static(publicDir));
-
 app.use(serveIndex(publicDir, { icons: true }));
 
 app.listen(port, () => {
