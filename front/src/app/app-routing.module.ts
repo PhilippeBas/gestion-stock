a31f14168df2da886130e5f07faf375a2a796b33
philippe basuyau <basuyau.philippe@free.fr>
2023-07-05 14:40:19 +0200

    dev routing step 4/4
    Ajout des liens
    Création des titres

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -7,10 +7,12 @@ const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    title: 'HomePage',
   },
   {
     path: 'legal',
     component: LegalComponent,
+    title: 'Mention légales',
   },
 ];
 
