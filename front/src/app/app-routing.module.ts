8495805b159bd8b64d6b490e129610cf35dc5b75
philippe basuyau <basuyau.philippe@free.fr>
2023-07-05 16:08:01 +0200

    dev: easyloading

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ const routes: Routes = [
     component: LegalComponent,
     title: 'Mention légales',
   },
+  { path: 'stock', loadChildren: () => import('./stock/stock.module').then(m => m.StockModule) },
 ];
 
 @NgModule({
