d2f6c4496d8bff09ed891115f5638f58794fb3db
philippe basuyau <basuyau.philippe@free.fr>
2023-07-06 10:47:22 +0200

    dev: creatione service

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -4,6 +4,7 @@ import {
   faRotateRight,
   faTrashCan,
 } from '@fortawesome/free-solid-svg-icons';
+import { Article } from '../interfaces/article';
 
 @Component({
   selector: 'app-stock',
@@ -14,4 +15,13 @@ export class StockComponent {
   faRotateRight = faRotateRight;
   faPlus = faPlus;
   faTrashCan = faTrashCan;
+  articles: Article[] = [
+    { id: 'a1', name: 'Tounevis', price: 25, qty: 500 },
+    { id: 'a2', name: 'Disqueuse', price: 250, qty: 50 },
+    { id: 'a3', name: 'Marteau', price: 55, qty: 69 },
+  ];
+
+  getArticleId(index: number, a: Article) {
+    return a.id;
+  }
 }
