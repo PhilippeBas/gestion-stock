b64424664b3ac43e55cd6745576783ef3885dde2
philippe basuyau <basuyau.philippe@free.fr>
2023-07-06 11:15:55 +0200

    Dev: bascule du tableau de service dans un observable

--- front/src/app/services/article.service.ts
+++ front/src/app/services/article.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Article } from '../interfaces/article';
+import { BehaviorSubject } from 'rxjs';
+
+const articles: Article[] = [
+  { id: 'a1', name: 'Tounevis', price: 25, qty: 500 },
+  { id: 'a2', name: 'Disqueuse', price: 250, qty: 50 },
+  { id: 'a3', name: 'Marteau', price: 55, qty: 69 },
+];
 
 @Injectable({
   providedIn: 'root',
 })
 export class ArticleService {
-  articles: Article[] = [
-    { id: 'a1', name: 'Tounevis', price: 25, qty: 500 },
-    { id: 'a2', name: 'Disqueuse', price: 250, qty: 50 },
-    { id: 'a3', name: 'Marteau', price: 55, qty: 69 },
-  ];
-
+  articles$ = new BehaviorSubject<Article[]>(articles);
   constructor() {}
 }
